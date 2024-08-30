77d182323a70e0bd7a810e6046195d2fc0f8f802
ChengHsunTai <137912642+ChengHsunTai@users.noreply.github.com>
2024-08-30 16:11:56 +0800

    Update lidarThread.cpp

--- CoLa2 InitializePose/lidarThread.cpp
+++ CoLa2 InitializePose/lidarThread.cpp
@@ -18,5 +18,27 @@ lidarThread::~lidarThread(){
 
 void lidarThread::openUDPsocket(){
     int port = 5010;
+
+}
+
+char *lidarThread::InitializedPose(int64_t x, int64_t y, int32_t heading){
+    const unsigned char *xstr = (const unsigned char *) &x;
+    const unsigned char *ystr = (const unsigned char *) &y;
+    const unsigned char *headingstr = (const unsigned char *) &heading;
+
+    char *xstring = (char *) malloc(4 * sizeof(char));
+    char *ystring = (char *) malloc(4 * sizeof(char));
+    char *headingstring = (char *) malloc(4 * sizeof(char)); 
+    for (int i = 0; i < 4; i++){
+        xstring[i] = xstr[3-i];
+        ystring[i] = ystr[3-i];
+        headingstring[i] = headingstr[3-i];
+    }
+    char pose[1024];
+    memcpy(&pose[0], xstring, 4);
+    memcpy(&pose[4], ystring, 4);
+    memcpy(&pose[8], headingstring, 4);
     
+    char *finalpose = &pose[0];
+    return finalpose;
 }
