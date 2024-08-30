ef59544e9164e789e038e7452e3b69c6e16166ba
ChengHsunTai <137912642+ChengHsunTai@users.noreply.github.com>
2024-08-30 16:11:37 +0800

    Update lidarThread.h

--- CoLa2 InitializePose/lidarThread.h
+++ CoLa2 InitializePose/lidarThread.h
@@ -7,8 +7,8 @@ public:
     ~lidarThread();
 
     void openUDPsocket();
-    void closeTCPsocket(int sock);
+    void closeUDPsocket(int sock);
+    char *InitializedPose(int64_t x, int64_t y, int32_t heading);
 private:
 
 };
-
